9a6249543be2e5eb5a255e8d7d6cf5386a4332bf
unknown <abood.alkhaldi004@gmail.com>
2026-03-30 20:41:56 +0300

    Feat : adding the integration event base at the Infra from BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks.EventBus/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.EventBus;
+
+public abstract record IntegrationEvent : IIntegrationEvent
+{
+    public Guid EventId { get; init; } = Guid.NewGuid();
+    public DateTime OccurrenceAt { get; init; } = DateTime.UtcNow;
+}
